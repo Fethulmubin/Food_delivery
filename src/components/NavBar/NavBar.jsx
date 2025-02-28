238d904c853c7e1bae4e9d7117dfb36abb6e9809
fethulmubin <fetihul@gmail.com>
2025-02-28 15:39:55 +0300

    cart page started to add names of food from home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({setShowLogin}) => {
     const[menu, setMenu] = useState('home')
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="log" />
+        <Link to='/'><img src={assets.logo} alt="" className="log" /></Link>
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>{setMenu('home')}} className={menu==='home'?'active':''}>Home</Link>
             <a href='#explore-menu' onClick={()=>{setMenu('menu')}} className={menu==='menu'?'active':''}>Menu</a>
@@ -17,7 +17,7 @@ const NavBar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className="dot"></div>
             </div>
             <button onClick={()=>{setShowLogin(true)}}>sign in</button>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
+// import { food_list } from '../../assets/assets/frontend_assets/assets'
 const Cart = () => {
-    <div>Cart</div>
+    const {cartItems, food_list, removeFromCart} = useContext(StoreContext)
+    return(
+    <div className='cart'> 
+        <div className="cart-items">
+            <div className="cart-items-title">
+                <p>Items</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Quality</p>
+                <p>Total</p>
+                <p>Remove</p>
+            </div>
+            <br />
+            <hr />
+            {food_list.map((item, index)=>{
+                if(cartItems[item._id] > 0){
+                    return(
+                        <div className='cart-item-title  cart-items-item' >
+                            {item.name}
+                        </div>
+
+                    )
+                }
+            })}
+        </div>
+
+    </div>
+    )
 }
-import './Cart.css'
+
 export default Cart
