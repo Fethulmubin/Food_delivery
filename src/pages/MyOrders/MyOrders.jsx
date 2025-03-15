3331220575c1ca05c0a05fab29c379e52d464d1e
fethulmubin <fetihul@gmail.com>
2025-03-15 11:53:28 +0300

    myorders fetching functionality

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './MyOrders.css'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
+import { set } from 'mongoose'
+
 
 const MyOrders = () => {
+
+    const [data, setData] = useState([]);
+    const {token, url} = useContext(StoreContext);
+
+    const fetchOrders = async () =>{
+        const response = await axios.post(`${url}api/order/userorders`, {}, {withCredentials: true});
+        if(response.data.success){
+            setData(response.data.orders)
+        }
+    }
+//fetching our orders
+    useEffect(()=>{
+        fetchOrders();
+    },[])
   return (
     <div>MyOrders</div>
   )
