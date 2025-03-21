b2bd38bd71669eda2d75afc63987a45564e7b7a7
fethulmubin <fetihul@gmail.com>
2025-03-21 09:25:04 +0300

    loading functionality for foodDisplay component

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,22 +3,19 @@ import './FoodDisplay.css'
 import {useContext} from 'react'
 import FoodItem from '../FoodItem/FoodItem'
 import { StoreContext } from '../../context/StoreContext'
+import Spinner from '../../../../admin/admin/src/components/Spinner/Spinner'
 const FoodDisplay = ({catagory}) => {
 
     const {food_list} = useContext(StoreContext)
   return (
     <div className='food-displa' id='food-display'>
         <h2>Top dishes near to you</h2>
-        <div className="food-display-list">
-            {food_list.map((item, index)=>{
+        <div className={food_list.length === 0 ? 'food-display-list-spin' : 'food-display-list'}>
+            {food_list.length === 0 ? <Spinner/> : food_list.map((item, index)=>{
               if(catagory==='All' || item.category===catagory) {
                 // {console.log(item.category)}
                   return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
               }
-              // else{
-              //       return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-              // }
-                // return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
                 
             })}
         </div>
